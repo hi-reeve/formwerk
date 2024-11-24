d2eb3a3e4bd5c625abcdfb112d98dad3b468e968
Abdelrahman Awad <logaretm1@gmail.com>
2024-11-24 22:59:40 +0200

    chore: added fake zod package to ecosystem test

---
+++ packages/ecosystem/src/zod/zod.spec.ts
@@ -0,0 +1,86 @@
+import { fireEvent, render, screen } from '@testing-library/vue';
+import { z } from 'zod';
+import { useForm } from '@formwerk/core';
+import { flush } from '@test-utils/index';
+
+test('zod schemas are supported', async () => {
+  const handler = vi.fn();
+  const schema = z.object({
+    email: z.string().email().optional().nullable(),
+    password: z.string().min(8),
+  });
+
+  await render({
+    setup() {
+      const { handleSubmit, getError, values } = useForm({
+        schema,
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      values.email;
+
+      // values.password.charAt;
+
+      return {
+        getError,
+        onSubmit: handleSubmit(v => {
+          handler(v.toObject());
+        }),
+      };
+    },
+    template: `
+      <form @submit="onSubmit" novalidate>
+        <span data-testid="form-err-1">{{ getError('email') }}</span>
+        <span data-testid="form-err-2">{{ getError('password') }}</span>
+
+        <button type="submit">Submit</button>
+      </form>
+    `,
+  });
+
+  await fireEvent.click(screen.getByText('Submit'));
+  await flush();
+  expect(screen.getByTestId('form-err-1').textContent).toBe('');
+  expect(screen.getByTestId('form-err-2').textContent).toBe('Required');
+  expect(handler).not.toHaveBeenCalled();
+});
+
+test('collects multiple errors per field', async () => {
+  const handler = vi.fn();
+  const schema = z.object({
+    test: z.string().email().min(8),
+  });
+
+  await render({
+    setup() {
+      const { getErrors, validate } = useForm({
+        schema,
+        initialValues: {
+          test: '123',
+        },
+      });
+
+      return {
+        onSubmit: async () => {
+          await validate();
+
+          handler(getErrors());
+        },
+      };
+    },
+    template: `
+      <form @submit="onSubmit" novalidate>
+        <button type="submit">Submit</button>
+      </form>
+    `,
+  });
+
+  await fireEvent.click(screen.getByText('Submit'));
+  await flush();
+  expect(handler).toHaveBeenCalledWith([
+    {
+      path: 'test',
+      messages: ['Invalid email', 'String must contain at least 8 character(s)'],
+    },
+  ]);
+});
