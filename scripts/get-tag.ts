c8d64e0d6eef42dd2105a200b87641127a1a7424
Abdelrahman Awad <logaretm1@gmail.com>
2024-11-28 02:36:41 +0200

    chore: remove unused scripts

--- scripts/get-tag.ts
+++
@@ -1,4 +0,0 @@
-import { version } from '../packages/core/package.json';
-import fs from 'fs';
-
-fs.writeFileSync('version.output.txt', `v${version}`);
--- scripts/release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pnpm build
-pnpm changeset version
-pnpm install
-git add .
-git commit -m "chore(release): publish"
-pnpm changeset publish
-git push --follow-tags
