dbaf982adf9638b0b0c8e2ad728e2ca05ccdb56b
Abdelrahman Awad <logaretm1@gmail.com>
2024-12-03 02:48:27 +0200

    feat: expose useLocale composable

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -13,6 +13,7 @@ export * from './useForm';
 export * from './useFormGroup';
 export * from './useFormRepeater';
 export * from './validation';
+export * from './i18n/useLocale';
 export { version } from './constants';
 export { normalizePath } from './utils/path';
 
